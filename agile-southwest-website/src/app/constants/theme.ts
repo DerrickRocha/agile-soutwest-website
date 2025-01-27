b900e98dc0636c34e7369d655bb7003bdd0d4055
Derrick Rocha <drocha616@gmail.com>
2025-01-27 12:32:20 -0700

    Add-Hero-Section (#3)
    * Added styling for home screen. Made header responsive.
    
    * Responsivenes.
    
    * Fixed positioning.
    
    * Added use client
    
    * Removed uneeded styling.
    
    * Code cleanup.
    
    * Adjusted positioning.
    
    ---------
    
    Co-authored-by: Derrick rocha <derrickrocha@Derricks-iMac.local>

--- agile-southwest-website/src/app/constants/theme.ts
+++ agile-southwest-website/src/app/constants/theme.ts
@@ -15,11 +15,14 @@ export const theme = {
         body: '"Roboto", sans-serif',
     },
     breakpoints: {
-        mobile: '768px',
-        tablet: '1024px',
-        desktop: '1200px',
+        mobile: '540px',
+        tablet: '768px',
+        smallDesktop: '1280px',
+        standardDesktop: '1440px',
+        largeDesktop: '1920px'
     },
     padding: {
+        verticalDivSpacing: '40px',
         gutter: '70px',
     },
     dimensions: {
@@ -29,11 +32,16 @@ export const theme = {
         navListBottomMargin: '16px'
     },
     textSize: {
-        heading1: '44px',
-        heading2: '28px',
+        heroHeading1Large: '71px',
+        heroHeading1Small: '48px',
+        heroHeading2Large: '48px',
+        heroHeading2Small: '32px',
+        heading1: '71px',
+        heading2: '48px',
         heading3: '18px',
         subHeading: '14px',
         body: '14px',
         links: '18px'
     }
+
 };
