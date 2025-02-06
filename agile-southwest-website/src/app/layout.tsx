78fa12242559049aa6256f19d151dfe9d3d16b7b
Derrick rocha <derrickrocha@Derricks-iMac.local>
2025-02-06 12:06:39 -0700

    Not using global css in home screen.

--- agile-southwest-website/src/app/layout.tsx
+++ agile-southwest-website/src/app/layout.tsx
@@ -1,23 +1,20 @@
-import type {Metadata} from "next";
-import "./globals.css";
-import Layout from "@/app/components/layout";
-import React from "react";
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-    title: "Home",
-    description: "Agile Southwest repair page",
+  title: "Home",
+  description: "Agile Southwest repair page",
 };
 
 export default function RootLayout({
-                                       children,
-                                   }: Readonly<{
-    children: React.ReactNode;
+  children,
+}: Readonly<{
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en">
-        <Layout>
-            {children}
-        </Layout>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
 }
