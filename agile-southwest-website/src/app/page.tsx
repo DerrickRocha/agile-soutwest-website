520ceb94d859659d5881f4cb17ece1dfb930c4cf
Derrick rocha <derrickrocha@Derricks-iMac.local>
2025-02-05 20:56:21 -0700

    Removed hero.

--- agile-southwest-website/src/app/page.tsx
+++ agile-southwest-website/src/app/page.tsx
@@ -3,7 +3,7 @@
 import Layout from "@/app/components/layout";
 import React from "react";
 import {theme} from "@/app/constants/theme";
-import Hero1Text, {Body, H1, Hero2Text} from "@/app/components/text";
+import  {Body, H1} from "@/app/components/text";
 import styled from "styled-components";
 import VerticalSpacingSmall from "@/app/components/verticalSpacingSmall";
 import VerticalSpacingLarge from "@/app/components/VerticalSpacingLarge";
@@ -21,64 +21,6 @@ function Home() {
             </PageWrapper>
 
         </Layout>
-    );
-}
-
-const HeroContainer = styled.div`
-    display: grid;
-    grid-template-columns: 1fr; 
-    grid-template-rows: 1fr; 
-    width: 100%;
-    height: 920px;
-`;
-
-const Overlay = styled.div`
-    grid-column: 1 / -1; 
-    grid-row: 1; 
-    background-color: rgba(0, 51, 102, 0.8);
-    width: 100%;
-    height: 100%; 
-`;
-
-const OverlayTextContainer = styled.div`
-    grid-column: 1 / -1; 
-    grid-row: 1; 
-    padding: 0 ${theme.padding.gutter};
-    display: flex;
-    flex-direction: column;
-    justify-content: center; 
-    align-items: start; 
-    text-align: left;
-
-   
-`;
-
-const FixedImage = styled.div`
-    grid-column: 1 / -1;
-    grid-row: 1; 
-    //background-image: url('/images/optimized_agile-methodology-machine-learning.webp');
-    background-size: cover;
-    background-position: center;
-    background-repeat: no-repeat;
-    width: 100%;
-    height: 100%;
-`;
-
-function HomeHeroSection() {
-    return (
-        <HeroContainer>
-            <FixedImage>
-            </FixedImage>
-            <Overlay/>
-            <OverlayTextContainer>
-                <Hero1Text fontFamily={theme.fonts.hero} color="white">
-                    Your Vision, <span style={{color: theme.colors.secondary}}>Our Expertise</span>
-                </Hero1Text>
-                <Hero2Text fontFamily={theme.fonts.hero} color="white">
-                    Flexible Web and Mobile Solutions for Growing Businesses
-                </Hero2Text>
-            </OverlayTextContainer>
-        </HeroContainer>
     );
 }
 
