d0bafed181497cf0afa8353d4a2fd6e96e542c43
Derrick rocha <derrickrocha@Derricks-iMac.local>
2025-02-08 02:23:29 -0700

    Added bottom images.

--- agile-southwest-website/src/app/page.tsx
+++ agile-southwest-website/src/app/page.tsx
@@ -4,6 +4,7 @@ import styling from "@/app/Home.module.css"
 import {theme} from "@/app/constants/theme";
 import VerticalSpacingSmall from "@/app/components/verticalSpacingSmall";
 import Card from "@/app/components/card";
+import Image from "next/image";
 
 export default function Home() {
     return (
@@ -11,6 +12,7 @@ export default function Home() {
             <HeroSection/>
             <Layout>
                 <ServicesSection/>
+                <VerticalSpacingLarge/>
                 <HomeAboutSection/>
                 <VerticalSpacingLarge/>
                 <HomeImagesSection/>
@@ -49,7 +51,7 @@ function ServicesSection() {
                 for Android & iOS, and expert PC,Mac, and electronics repair. We provide tailored solutions to help your
                 business grow, improve customer engagement, and ensure reliable device performance solutions.
             </text>
-            <VerticalSpacingSmall/>
+            <VerticalSpacingLarge/>
             <ServiceCards/>
         </>
     )
@@ -83,9 +85,67 @@ function ServiceCards() {
 }
 
 function HomeAboutSection() {
-    return (<></>);
+    return (
+        <div className={styling.aboutSectionDiv}>
+            <Image className={styling.aboutImage}
+                   width={350}
+                   height={350}
+                   src={"/images/home_about_section_image.webp"}
+                   alt={"About Section Image"}
+            />
+            <div className={styling.aboutTextDiv}>
+                <h1>Delivering Custom Solutions For Small Business Growth</h1>
+                <VerticalSpacingSmall/>
+                <text>
+                    At Agile Southwest, we are dedicated to helping small businesses thrive in
+                    the digital age by providing customized web development, native mobile app
+                    development, and electronics repair services. With years of experience serving
+                    businesses across the Southwest, we specialize in building responsive websites,
+                    SEO-optimized solutions, and user-friendly mobile apps tailored to your unique
+                    needs. Our team of experts is committed to delivering high-quality services that
+                    drive results, increase customer engagement, and enhance your overall online presence.
+                    Whether you’re looking for a complete website redesign, a robust Android or iOS app, or
+                    reliable PC, Mac, and electronics repairs, we’re here to turn your vision into reality.
+                    In addition to software solutions, we also provide professional soldering services for
+                    electronics, including power terminals and various components. Whether you&#39;re dealing with
+                    damaged power connections, broken terminals, or other small electronic repairs, our experienced
+                    team is equipped to help.
+                    We pride ourselves on offering personalized, cost-effective solutions that support your
+                    business goals. At Agile Southwest, our mission is to combine cutting-edge technology
+                    with a deep understanding of our clients&#39; needs to help them achieve lasting success.
+                    Let us be your partner in driving digital transformation, optimizing your business
+                    operations, and growing your brand.
+                </text>
+            </div>
+        </div>
+
+    );
 }
 
 function HomeImagesSection() {
-    return (<></>);
+    return (
+        <div className={styling.homeBottomImagesDiv}>
+            <Image
+                className={styling.homeBottomImage}
+                src="/images/neural_network.webp"
+                alt="Neural network image"
+                width={490}
+                height={450}
+            />
+            <Image
+                className={styling.homeBottomImage}
+                src="/images/website_development.webp"
+                alt="Web development image"
+                width={450}
+                height={490}
+            />
+            <Image
+                className={styling.homeBottomImage}
+                src="/images/soldering.webp"
+                alt="Web development image"
+                width={450}
+                height={490}
+            />
+        </div>
+    );
 }
