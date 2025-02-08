d754bd273fc3585cb08fc799feef412508fd51c8
Derrick rocha <derrickrocha@Derricks-iMac.local>
2025-02-07 23:54:38 -0700

    Removed logo from footer.

--- agile-southwest-website/src/app/components/footer.tsx
+++ agile-southwest-website/src/app/components/footer.tsx
@@ -1,24 +1,14 @@
 import React from "react";
-import Link from "next/link";
-import Image from "next/image";
 import styling from "@/app/Footer.module.css"
 
 export default function Footer() {
     return <footer className={styling.mainFooter}>
-        <Link href="/">
-            <Image
-                src="/images/agile_logo.svg"
-                alt="Agile Southwest Logo"
-                width={164}
-                height={80}
-            />
-        </Link>
+
         <div className={styling.copyrightContainer}>
             <text className={styling.footerText}>
                 &copy;2025 Agile Southwest. All rights reserved.
             </text>
         </div>
-
         <div className={styling.contactContainer}>
             <text className={styling.footerText}>
                 Agile Southwest LLC
