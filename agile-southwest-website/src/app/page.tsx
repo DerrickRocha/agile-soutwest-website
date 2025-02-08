ca7adcfb5926e83983a8481759d11ebfa41d000f
Derrick rocha <derrickrocha@Derricks-iMac.local>
2025-02-07 19:34:37 -0700

    Using css for hero.

--- agile-southwest-website/src/app/page.tsx
+++ agile-southwest-website/src/app/page.tsx
@@ -1,6 +1,8 @@
 import VerticalSpacingLarge from "@/app/components/VerticalSpacingLarge";
 import React from "react";
 import Layout from "@/app/components/layout";
+import styling from "@/app/Home.module.css"
+import {theme} from "@/app/constants/theme";
 
 export default function Home() {
     return (
@@ -18,9 +20,21 @@ export default function Home() {
 }
 
 function HeroSection() {
-    return <>
-        Greetings Lad
-    </>
+    return (
+        <div className={styling.heroContainer}>
+            <div className={styling.fixedImage}/>
+            <div className={styling.overlay}/>
+            <div className={styling.overlayTextContainer}>
+                <h1 className={styling.heroHeading}>
+                    Your Vision, <span style={{color: theme.colors.secondary}}>Our Expertise</span>
+                </h1>
+                <h1 className={styling.heroSubHeading}>
+                    Flexible Web and Mobile Solutions for Growing Businesses
+                </h1>
+            </div>
+        </div>
+    )
+
 }
 
 function ServicesSection() {
