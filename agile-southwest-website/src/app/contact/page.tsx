b5c3346112604329f5813ce324d4af65d507e98b
Derrick Rocha <drocha616@gmail.com>
2025-02-15 01:42:20 -0700

    ASW-2-Change-Website-Wording (#20)
    * ASW-2: Updated wording in home, about, and contact. Added new images.
    
    * ASW-2: Removed jpg images.
    
    ---------
    
    Co-authored-by: Derrick rocha <derrickrocha@Derricks-iMac.local>

--- agile-southwest-website/src/app/contact/page.tsx
+++ agile-southwest-website/src/app/contact/page.tsx
@@ -4,17 +4,16 @@ import VerticalSpacingSmall from "@/app/components/verticalSpacingSmall";
 import ContactForm from "@/app/components/client/contactForm";
 import Layout from "@/app/components/layout";
 
-const subHeading = "Reach Out to Agile Southwest for Custom Solutions and Support"
+const subHeading = "Reach Out to Derrick Rocha for Custom Solutions and Support"
 const content = [
-    "Have questions or need expert web development, mobile app solutions, or electronics repair services? Agile Southwest is here to help! Whether you’re looking for a custom website, a native Android or iOS app, or fast and reliable device repairs, our team is ready to assist. Contact us today to discuss your project, request a quote, or get expert support. Fill out the form below, call us, or send us a message—we’re here to provide the solutions your business needs to grow.",
-]
+    "Have questions or need expert web development, mobile app solutions, or electronics repair services? I’m here to help! Whether you're looking for a custom website, a native Android or iOS app, or fast and reliable device repairs, I provide personalized solutions tailored to your needs. Contact me today to discuss your project, request a quote, or get expert support. Fill out the form below, call, or send a message—I’m ready to help your business grow."]
 
 const ContactPage = () => {
 
     return (
         <Layout>
             <h1>
-                Contact Agile Southwest
+                Contact Derrick Rocha
             </h1>
             <VerticalSpacingLarge/>
             <InformationSection title={subHeading} content={content}/>
