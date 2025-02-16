60d48305388793ddb1e28d2d49a8c36e1951c8b7
Derrick rocha <derrickrocha@Derricks-iMac.local>
2025-02-15 18:39:09 -0700

    ASW-11: Added favicon.

--- agile-southwest-website/src/app/layout.tsx
+++ agile-southwest-website/src/app/layout.tsx
@@ -1,26 +1,29 @@
-import type { Metadata } from "next";
+import type {Metadata} from "next";
 import "@/app/globals.css";
 import React from "react";
 import Header from "@/app/components/header";
 import Footer from "@/app/components/footer";
 
 export const metadata: Metadata = {
-  title: "Home",
-  description: "Agile Southwest Home page",
+    title: "Home",
+    description: "Agile Southwest Home page",
+    icons: {
+        icon: "/favicon.png",
+    },
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body>
-      <Header/>
-          {children}
-      <Footer/>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+        <body>
+        <Header/>
+        {children}
+        <Footer/>
+        </body>
+        </html>
+    );
 }
