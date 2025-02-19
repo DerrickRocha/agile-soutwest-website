7e2b9e010c9435a9d0a3c7d238fa91e27fa10d76
Derrick rocha <derrickrocha@Derricks-iMac.local>
2025-02-19 12:25:14 -0700

    ASW-12: Changed about image. Updated logo description.

--- agile-southwest-website/src/app/layout.tsx
+++ agile-southwest-website/src/app/layout.tsx
@@ -6,7 +6,19 @@ import Footer from "@/app/components/footer";
 
 export const metadata: Metadata = {
     title: "Home",
-    description: "Agile Southwest Home page",
+    description: "My name is Derrick Rocha, and I'm a software engineer dedicated to providing" +
+        " custom web development, native mobile app development, and electronics repair services " +
+        "tailored to the unique needs of small businesses and individuals. " +
+        "Based in the Southwest, I bring technical expertise and a commitment to customer " +
+        "satisfaction, helping businesses and individuals navigate today’s digital landscape. " +
+        "With years of experience delivering responsive websites, user-friendly mobile apps, and " +
+        "reliable device repairs, I focus on offering cost-effective solutions that drive growth and " +
+        "improve efficiency. Whether you need a cutting-edge website to enhance your online presence, a " +
+        "mobile app to engage your audience, or expert repairs for PCs, Macs, and mobile devices, " +
+        "I’m here to support you every step of the way. I take pride in a hands-on, client-focused approach, " +
+        "ensuring that every project meets the highest standards of quality and performance. Let me be your " +
+        "trusted partner in navigating the ever-changing world of technology, from website design to mobile " +
+        "app development and device repair.",
     icons: {
         icon: "/favicon.png",
     },
